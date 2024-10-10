4bb37e5bd5b35cfcb9f19a780e9128a6985e65a1
Kaveesha Erandi <156656481+geeshanikaveesha@users.noreply.github.com>
2024-10-10 11:24:29 +0530

    Update DBconnect

--- src/main/webapp/WEB-INF/DBconnect.java
+++ src/main/webapp/WEB-INF/DBconnect.java
@@ -1,27 +1,28 @@
 package report;
+import java.sql.Connection; // Importing the Connection class from java.sql package, which is used to establish a connection to the database.
+import java.sql.DriverManager; // Importing DriverManager class that manages a set of JDBC drivers.
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-
-public class DBconnect {
+public class DBconnect { 
 	
-	
-	public static String url = "jdbc:mysql://localhost:3306/oop";
-	public static String user = "root";
-	public static String password = "Aa12345678";
+	// Database connection details
+	public static String url = "jdbc:mysql://localhost:3306/oop"; 
+	public static String user = "root"; 
+	public static String password = "Aa12345678"; 
 	public static Connection con;
 
+	
 	public static Connection getConnection() {
 		
 		try {
+			// Load the MySQL JDBC driver class
+			Class.forName("com.mysql.jdbc.Driver"); 
 			
-			Class.forName("com.mysql.jdbc.Driver");
 			
-			con = DriverManager.getConnection(url, user, password);
+			con = DriverManager.getConnection(url, user, password); 
 			
 		}
-		catch (Exception e) {
-			System.out.println("Database connection is not success!!!");
+		catch (Exception e) { 
+			System.out.println("Database connection is not success!!!"); 
 		}
 		
 		return con;
